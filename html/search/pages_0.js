b5bb8c1013416eb958102136f5a85d1f8c2dcc2b
Travis CI <travis@travis-ci.org>
2018-01-15 22:09:31 +0000

    Deploy code docs to GitHub Pages Travis build: 5
    Commit: fee460eeb55186f99541d1222656b9db30b4a5a8

--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_5famg88xx',['Adafruit_AMG88xx',['../md__home_travis_build_adafruit__adafruit__a_m_g88xx__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20amg88xx_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5famg88xx_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5famg88xx_29',['Adafruit AMG88xx Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_AMG88xx.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_AMG88xx)',['../md__home_travis_build_adafruit__adafruit__a_m_g88xx__r_e_a_d_m_e.html',1,'']]]
 ];
