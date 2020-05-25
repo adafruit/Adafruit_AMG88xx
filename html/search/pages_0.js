ab76fcfa06dc171296b0f64814f5719dea680e7f
Doxygen CI <travis@travis-ci.org>
2020-05-25 22:33:10 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['adafruit_20amg88xx_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5famg88xx_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5famg88xx_29',['Adafruit AMG88xx Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_AMG88xx.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_AMG88xx)',['../md__r_e_a_d_m_e.html',1,'']]]
-];
