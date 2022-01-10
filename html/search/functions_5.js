5a4a7836334cb848c6b976d9414f06ea7acdf425
Doxygen CI <ci-arduino@invalid>
2022-01-10 00:09:32 +0000

    Deploy docs to GitHub Pages from commit 52a632763c
    Commit: 52a632763c8d75818b479c29967616eb869ceb70
    GitHub Actions run: 1675205434

--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['readpixels',['readPixels',['../class_adafruit___a_m_g88xx.html#a465430ad61264933c69d1587bcf847c1',1,'Adafruit_AMG88xx']]],
+  ['readpixels',['readPixels',['../class_adafruit___a_m_g88xx.html#a9f79f1a9b1a1a8ae93c714e93cf7eaf0',1,'Adafruit_AMG88xx']]],
+  ['readpixelsraw',['readPixelsRaw',['../class_adafruit___a_m_g88xx.html#a646772eb62521904626447ab0b2c5ad9',1,'Adafruit_AMG88xx']]],
   ['readthermistor',['readThermistor',['../class_adafruit___a_m_g88xx.html#ad8f9d316451a8bfafc1fd38b579e613e',1,'Adafruit_AMG88xx']]]
 ];
